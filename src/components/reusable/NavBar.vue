d62ad050222e0a712f5faea0e2ace84f818f7cc6
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2024-04-20 16:13:32 +0530

    nav

--- src/components/reusable/NavBar.vue
+++ src/components/reusable/NavBar.vue
@@ -2,8 +2,8 @@
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="/assets/Logo.svg" class="h-8" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">QuickGone</span>
+                <img src="/assets/hr type logo.svg" class="h-8" alt="Flowbite Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
             </a>
             <div class="flex md:order-2">
                 <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search"
