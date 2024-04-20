99f154c8740c4a8951685037f21da2d928b49850
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2024-04-20 21:37:01 +0530

    added data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
-  <Navbar/>   
-<!--   
+  <Navbar />
+  <!--   
   <div class="p-8 mx-auto max-w-screen-xl mt-9 lg:py-12 lg:px-6">
     <div class="grid gap-8 lg:gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-end">
         <template v-for="item in item" :key="item">
@@ -8,18 +8,17 @@
                :item="item"
             />
         </template>
-    </div>
-    </div> -->
-    <div class="py-8 mx-auto max-w-screen-xl mt-8 lg:py-12 lg:px-6">
+</div>
+</div> -->
+  <div class="py-8 mx-auto max-w-screen-xl mt-8 lg:py-12 lg:px-6">
     <div class="grid gap-8 lg:gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-end">
       <a href="/">
-        <template v-for="item in item" :key="item">
-          <ProductCard
-                :item="item" />
+        <template v-for="item in item" :key="item.id">
+          <ProductCard :item="item" />
         </template>
       </a>
     </div>
-    </div>
+  </div>
 
 
 </template>
@@ -29,23 +28,26 @@ import ProductCard from '@/components/ProductCard.vue'
 import Navbar from '@/components/reusable/NavBar.vue'
 import { getitem } from '@/API/index.js'
 
-export default{
+export default {
   name: 'HomeView',
   components: {
     ProductCard,
     Navbar
   },
-  data(){
-        return {
-            item: [],
-        }
-    },
-    async mounted() {
-        await getitem().then((item) => {
-            // this.item = data.item;
-            console.log(item.item)
-        })
+  data() {
+    return {
+      item: [],
     }
+  },
+  async mounted() {
+    try {
+      const response = await getitem();
+      this.item = response.item;
+      console.log(this.item)
+    } catch (error) {
+      console.error('Error fetching items:', error);
+    }
+  }
 
 }
 </script>
