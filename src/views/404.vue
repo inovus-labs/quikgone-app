6181aa157bc1aae540732e943248c0d6c3848259
Badhusha <Badhusha.shaji22@example.com>
2024-04-21 10:15:11 +0530

    WD NAV bar

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,3 +1,16 @@
 <template>
+    <navbar />
     404
 </template>
+
+<script>
+import navbar from "@/components/reusable/NavBar.vue";
+    export default {
+        name: "404",
+        components: {
+            navbar
+        }
+    }
+</script>
+
+
